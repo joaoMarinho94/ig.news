75bebbf2a322518da19bb8fffcac186336d97d6b
joaoMarinho94 <joaopaulim94@gmail.com>
2021-12-07 16:18:48 -0300

    feat: create SignInButton component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { SignInButton } from './SignInButton';
 import styles from './styles.module.scss';
 
 export function Header(): JSX.Element {
@@ -9,6 +10,8 @@ export function Header(): JSX.Element {
           <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   );
