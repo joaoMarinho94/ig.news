c862e365ba33b9b4bc9ffe73ab03a4bee1c6eb23
joaoMarinho94 <joaopaulim94@gmail.com>
2021-12-07 16:39:19 -0300

    feat: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,26 @@
 import { NextHead } from '../components/NextHead';
 
+import styles from './home.module.scss';
+
 export default function Home(): JSX.Element {
   return (
     <>
-      <NextHead title="Início" />
-      <h1>olá Mundo</h1>
+      <NextHead title="Home" />
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          👋<span> Hey, welcome</span>
+          <h1>
+            News acout the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/img/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
