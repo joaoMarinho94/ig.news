c8e2cc2567469baa415b6cddbda5be6e16fac210
joaoMarinho94 <joaopaulim94@gmail.com>
2021-12-08 18:12:52 -0300

    feat: add save user after login

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -13,4 +16,24 @@ export default NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+            ),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+          )
+        );
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+  },
 });
--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -2,4 +2,5 @@ import { Client } from 'faunadb';
 
 export const fauna = new Client({
   secret: process.env.FAUNA_API_KEY,
+  domain: 'db.us.fauna.com',
 });
