a2e4917825d7d02b3d9c1294d824cafbe70d33b0
joaoMarinho94 <joaopaulim94@gmail.com>
2021-12-08 16:54:43 -0300

    feat: add next-auth with github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 import { Header } from '../components/Header';
 
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
 
