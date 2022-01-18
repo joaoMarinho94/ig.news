06120b689e2ba1decc3787dbea7952355de7dfb3
joaoMarinho94 <joaopaulim94@gmail.com>
2022-01-18 17:22:04 -0300

    feat: add links to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { SignInButton } from './SignInButton';
 import styles from './styles.module.scss';
 
@@ -7,8 +8,12 @@ export function Header(): JSX.Element {
       <div className={styles.headerContent}>
         <img src="/img/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts">
+            <a>Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
