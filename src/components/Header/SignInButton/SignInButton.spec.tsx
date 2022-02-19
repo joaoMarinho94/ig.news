0d8825731239a78bfc72713cb513add7f3ac2878
joaoMarinho94 <joaopaulim94@gmail.com>
2022-02-18 23:23:53 -0300

    test: add tests in SignInButton component

---
+++ src/components/Header/SignInButton/SignInButton.spec.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { render, screen } from '@testing-library/react';
+import { mocked } from 'ts-jest/utils';
+import { useSession } from 'next-auth/react';
+import { SignInButton } from './index';
+
+jest.mock('next-auth/react');
+
+describe('SignInButton Component', () => {
+  it('renders correctly when user is not autenticated', () => {
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce({ data: null, status: 'loading' });
+
+    render(<SignInButton />);
+
+    expect(screen.getByText('Sign in with Github')).toBeInTheDocument();
+  });
+
+  it('renders correctly when user is autenticated', () => {
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce({
+      data: {
+        user: {
+          email: 'john.doe@example.com',
+          name: 'John Doe',
+        },
+        expires: 'fake-expires',
+      },
+      status: 'authenticated',
+    });
+
+    render(<SignInButton />);
+
+    expect(screen.getByText('John Doe')).toBeInTheDocument();
+  });
+});
